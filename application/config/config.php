201d73d82fc65a7443fb427787a8fcef9da88638
tavo1981 <tavo1057@hotmail.com>
2015-12-22 09:46:17 -0500

    Definiendo url base

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/turismo/';
+$config['base_url'] = 'http://localhost/catastro_turistico/';
 
 /*
 |--------------------------------------------------------------------------
