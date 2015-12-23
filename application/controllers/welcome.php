1e016c5d16dbbd1670b9e8f5c3f7db4e985b9fcf
tavo1981 <tavo1057@hotmail.com>
2015-12-23 10:52:34 -0500

    CAMBIANDO URL BASE

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -6,15 +6,11 @@ class Welcome extends CI_Controller {
 	{
         $datos['nombre'] = 'juan';
         $this->load->helper('url');
-<<<<<<< HEAD
-        $this->load->view('a1',$datos);
-		$this->load->view('welcome_message');
-        $this->load->view('a2');
-=======
+
         $this->load->view('encabezado');
 		$this->load->view('welcome_message');
         $this->load->view('pie');
->>>>>>> e53336d2834be01ef2b509b7c5a29586f78d8027
+
 	}
 }
 
